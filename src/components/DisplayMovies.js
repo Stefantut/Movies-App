b720db7ae4fba2147d0f7ac9a0830e4b6d66bf70
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-05-17 06:20:08 +0100

    Added base url

--- src/components/DisplayMovies.js
+++ src/components/DisplayMovies.js
@@ -3,17 +3,19 @@ import fetchApi from "../api/fetchApi";
 
 const DisplayMovies = () => {
   const apiKey = process.env.REACT_APP_API;
-
+  const baseURL = "https://api.themoviedb.org/3";
   const response = fetchApi.useFetch(
-    `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}`
+    `${baseURL}/discover/movie?sort_by=popularity.desc&api_key=${apiKey}`
   );
+
   const movies = response.movies;
+
   if (!movies) {
     return <div>Loading...</div>;
   }
 
   const singleMovie = movies.map((movie, index) => (
-    <li key={`${movie.title}-${index}`} className="card">
+    <li key={`${movie.title}-${index}`} className={`card card--${index}`}>
       <p className="movie__title">{movie.title}</p>
     </li>
   ));
