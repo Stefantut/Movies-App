2c092a6e5804a1b159811a4e1be4a69677aaac1d
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-05-23 00:07:36 +0100

    Fixed Fetching

--- src/App.js
+++ src/App.js
@@ -1,39 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { API_KEY, BASE_URL } from "./Variables";
-import fetchApi from "./api/fetchApi";
 import SingleMovie from "./components/SingleMovie";
 
 function App() {
-  const response = fetchApi.useFetch(
-    `${BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`
-  );
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const movies = response.movies;
+  useEffect(() => {
+    const endpoint = `${BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
+    fetchMovies(endpoint);
+  }, []);
 
-  if (!movies) {
-    return <div>Loading...please wait</div>;
-  }
+  // FetchMovies function to be reused as many times we need
+  const fetchMovies = async (path) => {
+    setLoading(true);
+    try {
+      await fetch(path)
+        .then((result) => result.json())
+        .then((result) => setMovies(result.results));
+      setLoading(false);
+    } catch (error) {
+      console.log("Error Fetching Api:" + error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h2>Movies App</h2>
       </header>
-
       <ul className="all-movies">
         {/* Loop through all Movies and display each movie in a separate component  */}
-        {movies.map((movie, index) => (
-          <SingleMovie
-            key={movie.id}
-            index={index}
-            title={movie.title}
-            poster={movie.poster_path}
-            release={movie.release_date}
-          />
-        ))}
+        {movies &&
+          movies.map((movie, index) => (
+            <SingleMovie
+              key={movie.id}
+              index={index}
+              title={movie.title}
+              poster={movie.poster_path}
+              release={movie.release_date}
+            />
+          ))}
       </ul>
+      {loading && <div>Please wait... Loading</div>}
+
+      {/* <button onClick={handleClick}>Load</button> */}
     </div>
   );
 }
-
 export default App;
