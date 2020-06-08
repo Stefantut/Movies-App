a8952b8efb4ef67778d471a5bbdabfd6740322d4
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-06-08 22:20:04 +0100

    Animated movies

--- src/components/AllMovies.js
+++ src/components/AllMovies.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import SingleMovie from './singleMovie/SingleMovie'
 import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import '../css/animations.css'
+
 const StyledList = styled.ul`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
@@ -12,12 +13,11 @@ function AllMovies(props) {
     const { movies } = props;  // Destructure props in the function body
     return (
 
-        <TransitionGroup component={StyledList}>
-
+        <TransitionGroup component={StyledList} >
             {/* Loop through all Movies and display each movie in a separate component  */}
             {movies &&
                 movies.map((movie, index) => (
-                    <CSSTransition key={movie.id} timeout={300} classNames="fade">
+                    <CSSTransition key={movie.id} timeout={{ enter: 300, exit: 300 }} classNames="fade">
                         <SingleMovie
                             key={movie.id}
                             id={movie.id}
