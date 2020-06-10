fe46bc2400f0ecec93624f971c920ed41a296119
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-06-11 00:31:37 +0100

    Added validation before fetching

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,6 +5,7 @@ import styled from 'styled-components'
 function Search({ fetchMovies, searchState }) {
     const [searchTerm, setSearchTerm] = useState('');
     const [searched, setSearched] = useState(true);
+    const [error, setError] = useState('');
 
     const fetchSearchedMovies = (query) => {
         const endpoint = `${BASE_URL}/search/movie?query=${query}&api_key=${API_KEY}&language=en-US&page=1`;
@@ -18,20 +19,28 @@ function Search({ fetchMovies, searchState }) {
     }
 
     const handleClick = () => {
-        setSearched(true);
-        fetchSearchedMovies(searchTerm)
+        if (searchTerm.length >= 1) {
+            fetchSearchedMovies(searchTerm);
+            setError('');
+        } else {
+            setError('min char mandatory')
+        }
     }
 
     const handleKeyDown = (event) => {
         if (event.key === 'Enter') {
-            setSearched(true);
-            fetchSearchedMovies(searchTerm)
+            if (searchTerm.length >= 1) {
+                fetchSearchedMovies(searchTerm);
+                setError('');
+            } else {
+                setError('min char mandatory')
+            }
         }
     }
     return (
         <div>
             <input type="text" onChange={handleChange} value={searchTerm} onKeyDown={handleKeyDown} placeholder='Search Movie' />
-            <p onClick={handleClick}>Search</p>
+            <p onClick={handleClick}>Search</p><p className="error">{error}</p>
         </div>
 
     )
