7dcd1bef80911df1c8f7b948e20aa543938e77d9
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-06-11 01:43:41 +0100

    Fixed search to update state

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { API_KEY, BASE_URL } from "../Variables";
 import styled from 'styled-components'
 
 function Search({ fetchMovies, searchState }) {
     const [searchTerm, setSearchTerm] = useState('');
-    const [searched, setSearched] = useState(true);
+    const [searched, setSearched] = useState(false);
     const [error, setError] = useState('');
 
     const fetchSearchedMovies = (query) => {
         const endpoint = `${BASE_URL}/search/movie?query=${query}&api_key=${API_KEY}&language=en-US&page=1`;
-        searchState(searched, searchTerm);
         fetchMovies(endpoint);
     }
 
@@ -18,25 +17,33 @@ function Search({ fetchMovies, searchState }) {
         setSearchTerm(searchedTerm)
     }
 
+
     const handleClick = () => {
         if (searchTerm.length >= 1) {
+            setSearched(true);
             fetchSearchedMovies(searchTerm);
             setError('');
         } else {
-            setError('min char mandatory')
+            setError('Please type in min 1 character')
         }
     }
 
     const handleKeyDown = (event) => {
         if (event.key === 'Enter') {
             if (searchTerm.length >= 1) {
+                setSearched(true);
                 fetchSearchedMovies(searchTerm);
                 setError('');
             } else {
-                setError('min char mandatory')
+                setError('Please type min 1 character')
             }
         }
     }
+
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+        searchState(searched, searchTerm);
+    });
     return (
         <div>
             <input type="text" onChange={handleChange} value={searchTerm} onKeyDown={handleKeyDown} placeholder='Search Movie' />
