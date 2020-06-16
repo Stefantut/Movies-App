2a7020f509c78a95ca017c3123a07e475f5066a6
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-06-16 22:47:05 +0100

    Styled Not found page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,6 +1,38 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import TopBar from '../components/header/TopBar';
+import AppFooter from '../components/footer/AppFooter';
+import ReturnHome from '../components/buttons/ReturnHome';
+
+const StyledNotFound = styled.div`
+height:100vh;
+background-color: ${props => props.theme.colors.darkgrey};
+display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+
+const ContentSection = styled.section`
+  max-width: ${props => props.theme.content.medium};
+  margin: ${props => props.theme.spacers.xxlarge} auto;
+`
+const StyledMessage = styled.div`
+color: ${props => props.theme.colors.primary};
+font-size: ${props => props.theme.fontSizes.xxlarge};
+user-select: none;
+margin-bottom:  ${props => props.theme.spacers.xxxlarge};
+`
 
 const NotFound = () => (
-    <div><h1>NotFound</h1></div>
+    <StyledNotFound>
+        <TopBar />
+        <ContentSection>
+            <StyledMessage>Sorry!
+             Page Not Found!</StyledMessage>
+            <ReturnHome />
+        </ContentSection>
+        <AppFooter />
+    </StyledNotFound>
 )
 export default NotFound
