66375cde8d21339592620e412543fcf01cf8ee82
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-06-28 02:46:58 +0100

    Added tooltip - html title tag

--- src/components/singleMovie/SingleMovie.js
+++ src/components/singleMovie/SingleMovie.js
@@ -1,29 +1,44 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-import styled from 'styled-components'
+import styled from "styled-components";
 
 import PosterImage from "./PosterImage";
 import MovieTitle from "./MovieTitle";
 import { IMAGE_URL, POSTER_SIZE } from "../../Variables";
 import ReleaseDate from "./ReleaseDate";
-import fallbackPoster from '../../images/fallback-poster.jpg'
-
+import fallbackPoster from "../../images/fallback-poster.jpg";
 
 const StyledListItem = styled.li`
-list-style-type: none;
-display: flex;
-flex-direction: column;
-align-items: center;
-padding: ${props => props.theme.spacers.large} 0;
-`
-function SingleMovie({ index, id, poster, title, release, genresIds, genresList }) {
+  list-style-type: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: ${(props) => props.theme.spacers.large} 0;
+`;
+function SingleMovie({
+  index,
+  id,
+  poster,
+  title,
+  release,
+  genresIds,
+  genresList,
+}) {
   return (
-    <StyledListItem className={`card card--${index}`}>
-      <Link to={`movies/${id}`} >
+    <StyledListItem
+      className={`card card--${index}`}
+      title="Click to find out more"
+    >
+      <Link to={`movies/${id}`}>
         <PosterImage
-          url={(poster === null) ? fallbackPoster : `${IMAGE_URL}${POSTER_SIZE}${poster}`}
-          genresIds={genresIds} genresList={genresList}
+          url={
+            poster === null
+              ? fallbackPoster
+              : `${IMAGE_URL}${POSTER_SIZE}${poster}`
+          }
+          genresIds={genresIds}
+          genresList={genresList}
         />
       </Link>
       <MovieTitle title={title} />
@@ -39,4 +54,3 @@ SingleMovie.propTypes = {
   release: PropTypes.string,
 };
 export default SingleMovie;
-
